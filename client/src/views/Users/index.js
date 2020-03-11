8aeded9bd56a305554f9dbba5ca091c819a3a63b
= <=>
2020-03-11 06:50:14 -0700

    stylized users table

--- client/src/views/Users/index.js
+++ client/src/views/Users/index.js
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import {
+  Typography,
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody
+} from "@material-ui/core";
+
 const Users = () => {
   const [users, setUsers] = useState([]);
 
@@ -8,41 +19,51 @@ const Users = () => {
     axios.get(`${process.env.REACT_APP_API_URL}/users`).then(response => {
       let data = response.data;
       data.sort((a, b) => {
-        return a.name > b.name;
+        if (a.name > b.name) return 1;
+        if (a.name < b.name) return -1;
+        return 0;
       });
       setUsers(data);
     });
   }, []);
 
   return (
-    <div>
-      <h2>Users</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>&nbsp;</th>
-            <th>Name</th>
-            <th>Op?</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => (
-            <tr>
-              <td>
-                <img
-                  src={`https://crafatar.com/avatars/${user.uuid}`}
-                  alt={user.name}
-                  width="20"
-                  height="20"
-                />
-              </td>
-              <td>{user.name}</td>
-              <td>{user.level && user.level > 0 ? "Yes" : "No"}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <Typography variant="h5" component="h2">
+        Whitelisted Users
+      </Typography>
+      <TableContainer component={Paper}>
+        <Table aria-label="Users">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Op?</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map(user => (
+              <TableRow key={user.name}>
+                <TableCell scope="row">
+                  <div style={{ display: "flex" }}>
+                    <img
+                      src={`https://crafatar.com/avatars/${user.uuid}`}
+                      alt={user.name}
+                      width="20"
+                      height="20"
+                      style={{ marginRight: 8 }}
+                    />
+                    {user.name}
+                  </div>
+                </TableCell>
+                <TableCell>
+                  {user.level && user.level > 0 ? "Yes" : "No"}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
