10711770ba5db30ecc8d5316a226c2ba1e5347c9
= <=>
2020-03-11 06:10:05 -0700

    adds dashboard styling

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -1,15 +1,26 @@
 import React from "react";
 
+import { CssBaseline, AppBar, Toolbar, Typography, Container } from "@material-ui/core";
+
 import Users from "./Users";
 
 const Dashbaord = () => {
   const user = JSON.parse(localStorage.getItem("user"));
   return (
-    <div>
-        <h1>minecraft.rubyrepro.com</h1>
+    <>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            minecraft.rubyrepro.com
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
         <p>Welcome {user.name}</p>
-      <Users />
-    </div>
+        <Users />
+      </Container>
+    </>
   );
 };
 
