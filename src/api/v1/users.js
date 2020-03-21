c1f7e72303123a96e129b04db6508cbc7cd742e2
= <=>
2020-03-20 17:36:33 -0700

    updates uuid to be formatted with dashes when inserting into whitelist.json

--- src/api/v1/users.js
+++ src/api/v1/users.js
@@ -14,33 +14,41 @@ users.post("/", function(req, res) {
   const users = usersService.getAll();
 
   let errors = [];
-  if (!req.body.name) {
+  let name = req.body.name;
+  let uuid = req.body.uuid;
+  if (!name) {
     errors.push({
       field: "name",
       message: "is required"
     });
   }
-  if (!req.body.uuid) {
+  if (!uuid) {
     errors.push({
       field: "uuid",
       message: "is required"
     });
   }
+
   if (errors.length) {
     res.status(400).send({ message: "validation errors", errors });
     return;
   }
 
-  // make sure user does not already exist
-//   const user = users.find(
-//     user => user.uuid.replace(/-/g, "") === req.body.uuid.replace(/-/g, "")
-//   );
-//   if (user) {
-//     res.status(400).send({ message: `User "${req.body.name}" already exists!` });
-//     return;
-//   }
+  // make sure uuid is formatted with dashes, required by whitelist.json
+  if (uuid.legnth === 32) {
+    uuid =
+      uuid.substr(0, 8) +
+      "-" +
+      uuid.substr(8, 4) +
+      "-" +
+      uuid.substr(12, 4) +
+      "-" +
+      uuid.substr(16, 4) +
+      "-" +
+      uuid.substr(20);
+  }
 
-  // insert user
+  // insert/update user
   usersService
     .save({
       name: req.body.name,
@@ -54,7 +62,5 @@ users.post("/", function(req, res) {
       res.status(400).send(error);
     });
 });
-
-users.put("/", function(req, res) {});
 
 module.exports = users;
